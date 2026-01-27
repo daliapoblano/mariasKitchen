71e3feff9472c39947cc5e3994f404b1e32fa327
Dalia Barranco <DALIABARRANCO16@GMAIL.COM>
2026-01-26 23:28:13 -0500

    Added function to help with the scrolling of the reviews. Debugged an error in html file related to a button

--- js/index.js
+++ js/index.js
@@ -5,9 +5,46 @@ const leftArrow = document.querySelector('.left-arrow');
 const rightArrow = document.querySelector('.right-arrow');
 const reviewsContainer = document.querySelector('.reviews-container');
 
-let index = 0;
+let currentIndex = 0;
 
 //This function updates which review card is actively being read by users
 function activatingCard(index){
-    
+    reviewCards.forEach(card => {
+        card.classList.remove('featured');
+    })
+    reviewCards[index].classList.add('featured');
+
+    dots.forEach(dot => {
+        dot.classList.remove('active');
+    })
+    dots[index].classList.add('active');
+
+    const cardWidth = reviewCards[0].offsetWidth;
+    const gap = 32;
+    const scrollPosition = (cardWidth + gap) * index;
+
+    reviewsContainer.scrollTo({
+        left: scrollPosition,
+        behavior:'smooth'
+    });
 }
+
+//Button Activation
+rightArrow.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % reviewCards.length;
+    activatingCard(currentIndex);
+});
+leftArrow.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % reviewCards.length;
+    activatingCard(currentIndex);
+});
+
+//Dot Clicks
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentIndex = index;
+        activatingCard(currentIndex);
+    });
+});
+
+activatingCard(0);
