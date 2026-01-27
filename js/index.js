f515565fb039376962b3e8b523913cc9478b9510
Dalia Barranco <DALIABARRANCO16@GMAIL.COM>
2026-01-26 21:01:27 -0500

    Created variables for the elements needed to make the reviews section work. Added more css styling.

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//Reviews Section 
+const reviewCards = document.querySelectorAll('.review-card');
+const dots = document.querySelectorAll('.dot');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+const reviewsContainer = document.querySelector('.reviews-container');
+
+let index = 0;
+
+//This function updates which review card is actively being read by users
+function activatingCard(index){
+    
+}
